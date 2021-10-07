62fc5cbb991ddab31e33f1dc017885344d7d14d0
öner dogan <onerdogan91@gmail.com>
2021-10-07 23:36:11 +0300

    first commit

---
+++ src/test/java/day04/Odev.java
@@ -0,0 +1,20 @@
+package day04;
+
+public class Odev {
+    public static void main(String[] args) {
+
+//
+//        Mehmet Bulutluoz  11:23 PM
+//        1. “https://www.saucedemo.com” Adresine gidin
+//        2. Username kutusuna “standard_user” yazdirin
+//        3. Password kutusuna “secret_sauce” yazdirin
+//        4. Login tusuna basin
+//        5. Ilk urunun ismini kaydedin ve bu urunun sayfasina gidin
+//        6. Add to Cart butonuna basin
+//        7. Alisveris sepetine tiklayin
+//        8. Sectiginiz urunun basarili olarak sepete eklendigini control edin
+//        9. Sayfayi kapatin
+//        saucedemo.comsaucedemo.com
+//        Swag Labs
+    }
+}
